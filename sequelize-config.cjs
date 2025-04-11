e36af2d022fdf7c81d13f37d7132aa6e511cd2aa
Marcos Schlick <marcosschlick@gmail.com>
2025-04-11 17:20:35 -0300

    adicionar arquivos para configuração do `sequelize`

---
+++ sequelize-config.cjs
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    // <-- Adicione esta camada
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  },
+};
