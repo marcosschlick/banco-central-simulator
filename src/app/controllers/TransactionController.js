89cb0cb5f830a6d04b1e616fec2deb6abc53aa25
Marcos Schlick <marcosschlick@gmail.com>
2025-04-17 12:11:52 -0300

    fix: consertar as rotas do model `Transaction`

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -16,7 +16,9 @@ export default class TransactionController {
 
   getById = async (req, res) => {
     try {
-      const transaction = await this.transactionService.findById(req.params.id);
+      const transaction = await this.transactionService.findById(
+        req.params.transactionId,
+      );
       res.status(200).json(transaction);
     } catch (error) {
       res.status(404).json({ error: "Transaction not found" });
@@ -69,7 +71,7 @@ export default class TransactionController {
   update = async (req, res) => {
     try {
       const updatedTransaction = await this.transactionService.update(
-        req.params.id,
+        req.params.transactionId,
         req.body,
       );
       res.status(200).json(updatedTransaction);
@@ -80,7 +82,7 @@ export default class TransactionController {
 
   delete = async (req, res) => {
     try {
-      await this.transactionService.delete(req.params.id);
+      await this.transactionService.delete(req.params.transactionId);
       res.status(204).send();
     } catch (error) {
       res.status(404).json({ error: "Transaction not found" });
