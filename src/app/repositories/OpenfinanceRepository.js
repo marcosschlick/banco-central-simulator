c72ad17a82cd09ea461e200e59a4131b92181770
Marcos Schlick <marcosschlick@gmail.com>
2025-05-30 16:15:24 -0300

    feat: criar OpenfinanceRepository para autorizações

---
+++ src/app/repositories/OpenfinanceRepository.js
@@ -0,0 +1,34 @@
+import { OpenFinanceAuthorization as OpenFinance } from "../models/OpenFinanceAuthorization.js";
+
+export default class OpenFinanceRepository {
+  async create(openFinanceData) {
+    return await OpenFinance.create(openFinanceData);
+  }
+
+  async findById(id) {
+    return await OpenFinance.findByPk(id);
+  }
+
+  async findAll() {
+    return await OpenFinance.findAll();
+  }
+
+  async findByAccount(accountId) {
+    return await OpenFinance.findOne({
+      where: { account_id: accountId },
+    });
+  }
+
+  async update(id, updateData) {
+    const [, [updatedOpenFinance]] = await OpenFinance.update(updateData, {
+      where: { id },
+      returning: true,
+    });
+    return updatedOpenFinance;
+  }
+
+  async delete(id) {
+    const deletedRows = await OpenFinance.destroy({ where: { id } });
+    return deletedRows > 0;
+  }
+}
