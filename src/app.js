4048f6ce1ccc32464011518d90b139437bb82a77
Marcos Schlick <marcosschlick@gmail.com>
2025-06-02 09:25:19 -0300

    chore(app): atualizar configuração principal

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 import express from "express";
-import { accountRoutes } from "./routes/AccountRoutes.js";
-import { institutionRoutes } from "./routes/InstitutionRoutes.js";
-import { transactionRoutes } from "./routes/TransactionRoutes.js";
-import { userRoutes } from "./routes/UserRoutes.js";
+import { openFinanceRoutes } from "./routes/OpenFinanceRoutes.js";
 
 const app = express();
 app.use(express.json());
 
-app.use("/accounts", accountRoutes);
-app.use("/institutions", institutionRoutes);
-app.use("/transactions", transactionRoutes);
-app.use("/users", userRoutes);
+app.use("/openfinance", openFinanceRoutes);
 
 export default app;
