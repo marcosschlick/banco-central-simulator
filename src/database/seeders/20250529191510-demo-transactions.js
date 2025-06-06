f0fe86f39a3fd4394c64e50c007ef33b3e0b34da
Marcos Schlick <marcosschlick@gmail.com>
2025-06-06 19:01:32 -0300

    refactor: atualiza transações para novas contas

--- src/database/seeders/20250529191510-demo-transactions.js
+++ src/database/seeders/20250529191510-demo-transactions.js
@@ -5,19 +5,43 @@ export default {
       [
         {
           amount: 100.0,
-          account_id: 1, // conta do João
+          account_id: 1, // João Almeida
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           amount: 200.0,
-          account_id: 2, // conta da Maria
+          account_id: 2, // Maria de Freitas
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           amount: 300.0,
-          account_id: 3, // conta do Carlos
+          account_id: 3, // Gabriel Macedo
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          amount: 150.0,
+          account_id: 4, // Fernanda da Silva
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          amount: 250.0,
+          account_id: 5, // Rodrigo Caetano
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          amount: 350.0,
+          account_id: 6, // Renato Gaúcho
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          amount: 450.0,
+          account_id: 7, // Mazembe Mundial
           created_at: new Date(),
           updated_at: new Date(),
         },
