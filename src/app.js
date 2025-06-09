1caf4920b43cdf368a91888bba855bf3bd1ee223
Marcos Schlick <marcosschlick@gmail.com>
2025-06-09 11:11:59 -0300

    chore: adiciona configuração de diretório público

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { openFinanceRoutes } from "./routes/OpenFinanceRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/openfinance", openFinanceRoutes);
 
